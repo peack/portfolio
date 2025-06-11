c7c3f611cdb13c6f5c5c0b83248abb528984e7ed
peack <poliouxx@gmail.com>
2025-06-11 10:53:19 +0200

    Implement code changes to enhance functionality and improve performance

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/main.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+// import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as Element).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
